bf0056311eec3c767bf194fbdbc8a60eb00bf6af
arthor13 <arthor5games@gmail.com>
2025-09-18 10:03:57 -0300

    estudo no youtube

--- first_code.cpp
+++ first_code.cpp
@@ -1,5 +1,20 @@
+//Biblioteca padrão do C++, necessário para os comandos básicos
 #include <iostream>
+//Biblioteca utilizada para usar o setprecision, que basicamente determina o numero de algarismos em um número flutuante
 #include <iomanip>
+/* 
+    Estou usando um curso da udemy e um curso do youtube ->https://www.youtube.com/watch?v=-TkoO8Z07hI 
+*/
+
+namespace first
+{
+    int x = 1;
+}
+namespace second
+{
+    int x = 2;
+}
+
 
 int main()
 {
@@ -34,4 +49,50 @@ int main()
    std::string lista[] = {"Lista", "em", "C++"};
     std::cout << lista[0] << std::endl;
     lista[0] = "Teste";
-}
+
+    //Constante em C++ (Standard da indústria é utilizar UPPERCASE para variáveis constantes)
+    const double PI = 3.14159;
+    double radius = 10;
+    double circumferaence = 2 * PI * radius;
+
+
+    /*
+    
+            Namespace -> Declarar fora da MAIN. É possivel ter variáveis de mesmo nome com diferentes valores.
+            Caso eu tenho um namespace e uma variável local com o mesmo nome e printar, caso eu não especifique que estou referenciado a do namespace ele irá pegar a  variável local.
+            É possível digitar *using namespace namespace_name* no ínicio da MAIN, dessa forma dentro daquele escopo eu não precisaria referenciar o nome daquele namespace toda vez que fosse digitar a variável. EX.: *first::x* -> *x*
+            INTERESSANTE!!!
+                É possível utilizar *using namespace std* retirando a necessidade de utilizar *std::* antes de funções como *cout* ou variáveis como *string*
+                O problema é que *std* possui diversos nomes, e isso provavelmente geraria um possível conflito nas variáveis.
+                    Solução -> *using std::cout* *using std::string*
+                        Eu creio que seja melhor utilizar *std::* normalmente do que ficar utilizando esse namespace que pode gerar altas dores de cabeça.
+     */
+    int x = 5;
+    std::cout << x << std::endl;
+    std::cout << first::x << std::endl;
+
+    /*
+        TYPEDEf -> parece ser semelhante a C.
+        Parece que é senso comum terminar o data type do typedef com _t. Ex: *typedef int inteiro_t* 
+        Outra forma mais popular de utilizar typedef:
+            *using inteiro_t = int;*
+    
+    */
+
+    /*
+        Conversão de tipos:
+            Implicit0
+                *int x = 3.14*
+            Explicito
+                *double = (int) 3.14*    
+    */
+
+
+    /*
+        INPUT:
+            *std::cin >> variable_name*
+            *std::getline(std::cin >> std::ws, variable_name)* <- Para strings com possíveis whitespaces.
+    */
+    
+
+    }
