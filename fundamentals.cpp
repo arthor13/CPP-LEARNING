094d52119dbe724878a4f6539fcc0507fbfee312
arthor13 <arthor5games@gmail.com>
2025-09-19 11:31:56 -0300

    continuação do dia 19 -> inputs

--- fundamentals.cpp
+++ fundamentals.cpp
@@ -6,6 +6,10 @@
 #include <vector>
 //Biblioteca pra utilizar maps
 #include <map>
+// Para utilizar getline()
+#include <string>
+//Para limpar literalmente todo o buffer de input é preciso importar essa library
+#include <limits>
 /* 
     Estou usando um curso da udemy chamado *Learn C++ and make a game from scratch* e um curso do youtube -> https://www.youtube.com/watch?v=-TkoO8Z07hI 
     Quando vc ver algo entre ** nos comentários é basicamente o código que voce digitaria, tem vezes que eu utilizo elas e tem vezes que não 
@@ -50,10 +54,51 @@ int main()
    std::cout << "Size of bool: " << sizeof(bool) << "bytes" << std::endl;
    std::cout << "Size of string: " << sizeof(std::string) << "bytes" << std::endl;
     
+
+    /*
+        INPUT:
+            *std::cin >> variable_name*
+            *std::getline(std::cin, variable_name) -> para pegar uma string em C++, se não utilizar ela a sua string vai ser cortado no primeiro espaço que voce der
+
+            INTERESSANTE ->
+                ASSIM COMO VÁRIAS SITUAÇÕES EM C++ QUE NORMALMENTE ACUSARIAM ERRO EM OUTRA LINGUAGEM, PORÉM AQUI NÃO ACUSA É A QUESTÃO DO INPUT DE DADO ERRADO
+                    O QUE EU QUERO DIZER É:
+                        VAMOS SUPOR QUE VOCÊ INSIRA UM CARACTER "T" QUALQUER EM UMA VARIÁVEL QUE É DO TIPO INTEIRO,
+                        AO INVÉS DO PROGRAMA DAR UM ERRO, O C++ IRÁ ADICIONAR POR DEFAULT O VALOR 0 À SUA VARIÁVEL INTEIRA
+                        E O SEU CARACTER "T" QUE VOCE ADICIONOU IRÁ PARA O BUFFER DE INPUT, O QUE VAI FODER TODOS OS SEUS PRÓXIMOS INPUTS
+                        VOCÊ PODE UTILIZAR ENTÃO UM std:cin.clear(); QUE IRÁ LIMPAR O BUFFER DE INPUT 
+                        OU SEJA, VOCÊ VAI CONSEGUIR FAZER OUTRAS INPUTS DEPOIS DO INPUT ERRADO, 
+                        PORÉM AQUELE VALOR ERRADO QUE VOCÊ ADICIONOU AINDA VAI ESTAR NO BUFFER E SERÁ ASSOCIADO AO PRÓXIMO INPUT QUE VOCÊ FIZER. 
+                        OU SEJA, A FUNCÃO cin.clear() É COMO SE ESTIVESSE EMPURRANDO AQUILO QUE ESTÁ NO BUFFER DE INPUT PARA 
+                        A PRÓXIMA VARIÁVEL QUE VC PUDESSE RECEBER 
+                            EX: 
+                              std::cin >> numeroInteiro;
+                              std::cin.clear()
+                              std::cin >> variavelString;
+
+                            CASO O USUÁRIO INSIRA A LETRA "t" NO numeroInteiro, VOCÊ NÃO IRÁ CONSEGUIR INSERIR NADA NA variavelString 
+                            E O VALOR DE "t" SERÁ AUTOMATICAMENTE ASSOCIADO À variavelString, PORÉM OS PRÓXIMOS INPUTS SERIAM NORMAIS
+                                        -------------------------------------------------------------------------------
+                            CASO VOCÊ NEM UTILIZASSE O std::cin.clear() VOCÊ NÃO CONSEGUIRIA INSERIR NADA NO variavelString E ELA AINDA IA FICAR COM VALOR NULO
+                            E O "t" AINDA IA ESTAR NO BUFFER, FERRANDO COM TODOS SEUS INPUTS SEGUINTES
+
+                        PARA RESOLVER TODOS ESSAS SITUAÇÕES PODE SE UTILIZAR O std::cin.clear() SEGUIDO DO std::cin.ignore(numero_de_inputs_a_ser_ignorados_no_buffer, '\n');
+                        O ignore() BASICAMENTE VAI IGNORAR TODAS OS PRÓXIMO N INPUTS QUE VOCÊ DEFINIU NO PRIMEIRO PARAMETRO
+                        E VAI PARAR DE IGNORAR DEPOIS DE UMA QUEBRA DE LINHA, QUE ADICIONAMOS NO SEGUNDO PARAMETRO.
+                        É IMPORTANTE ADICIONAR A QUEBRA DE LINHAS, POIS CASO NÃO ADICIONEMOS TODOS OS PRÓXIMOS N INPUTS SERÃO IGNORADOS MESMO QUE O INPUT ERRADO
+                        DO USUÁRIO JA TENHA SIDO DESCARTADO. 
+                        DICA: É BOM COLOCAR UM NÚMERO BEM ALTO NO PRIMEIRO PARAMETRO, POIS NUNCA SE SABE QUAIS ATROCIDADES O USUÁRIO COLOCOU NO INPUT. 
+                              OU SEJA, SE VOCÊ COLOCAR 5 NO PRIMEIRO PARAMETRO E O USUÁRIO DIGITOU 7 CARACTERES, VÃO FICAR 2 CARACTERES INDESEJADOS NO BUFFER
+                                EXISTE UMA FUNÇÃO UTILIZANDO A BIBLIOTECA <limits> QUE É std::numeric_limits<std::streamsize>::max() 
+                                E SE VOCÊ COLOCAR ELA NO PRIMEIRO PARAMETRO DA cin.ignore() TODO O BUFFER SERÁ IGNORADO, INDEPENDENTE DO TAMANHO DO INPUT DO USUÁRIO
+                                !!!! LEMBRE SEMPRE DE COLOCAR O '\n' NO SEGUNDO PARAMETRO (QUE É O DE PARADA), PORQUE SE NÃO VOCÊ PODE IGNORA OS PRÓXIMOS INPUTS DO PROGRAMA 
+    */
+    
    /*
         Array em C++
 
-        Para pegar o tamanho de uma array basta utilizar o sizeof da array e dividir o sizeof do tipo de dado dela -> sizeof(name_of_array)/sizeof(data_type_of_the_array)
+        Para pegar o tamanho de uma array basta utilizar o sizeof da array e dividir o sizeof pelo tipo de dado dela
+            sizeof(name_of_array)/sizeof(data_type_of_the_array)
         Arrays não mudam de tamanho
 
    */
@@ -66,9 +111,11 @@ int main()
         Vector em C++
                 FUN FACT -> VECTOR EM C++ FOI CRIAdO PRIMEIRO DO QUE A ARRAY
 
-            Para buscar o primeiro valor de um vector basta utilizar -> vector.name_front() 
+            Para buscar o primeiro valor de um vector basta utilizar -> 
+                vector.name_front() 
             Para buscar o último valor de um vector basta utilizar -> vector_name.back()
-            Para buscar o tamanho de um vector basta utilizar -> vector_name.size()
+            Para buscar o tamanho de um vector basta utilizar -> 
+                vector_name.size()
             O verdadeiro valor de um vector sobre uma array é a habilidade de diminuir e aumentar o tamanho do vector, o que não é possivel na array.
             Para aumentar (tipo append em pytohn):
                 vector_name.push_back(dado_a_ser_adicionado)
@@ -166,14 +213,14 @@ int main()
 
     /*
     
-            Namespace -> Declarar fora da MAIN. É possivel ter variáveis de mesmo nome com diferentes valores.
-            Caso eu tenho um namespace e uma variável local com o mesmo nome e printar, caso eu não especifique que estou referenciado a do namespace ele irá pegar a  variável local.
-            É possível digitar *using namespace namespace_name* no ínicio da MAIN, dessa forma dentro daquele escopo eu não precisaria referenciar o nome daquele namespace toda vez que fosse digitar a variável. EX.: *first::x* -> *x*
-            INTERESSANTE!!!
-                É possível utilizar *using namespace std* retirando a necessidade de utilizar *std::* antes de funções como *cout* ou variáveis como *string*
-                O problema é que *std* possui diversos nomes, e isso provavelmente geraria um possível conflito nas variáveis.
-                    Solução -> *using std::cout* *using std::string*
-                        Eu creio que seja melhor utilizar *std::* normalmente do que ficar utilizando esse namespace que pode gerar altas dores de cabeça.
+        Namespace -> Declarar fora da MAIN. É possivel ter variáveis de mesmo nome com diferentes valores.
+        Caso eu tenho um namespace e uma variável local com o mesmo nome e printar, caso eu não especifique que estou referenciado a do namespace ele irá pegar a  variável local.
+        É possível digitar *using namespace namespace_name* no ínicio da MAIN, dessa forma dentro daquele escopo eu não precisaria referenciar o nome daquele namespace toda vez que fosse digitar a variável. EX.: *first::x* -> *x*
+        INTERESSANTE!!!
+            É possível utilizar *using namespace std* retirando a necessidade de utilizar *std::* antes de funções como *cout* ou variáveis como *string*
+            O problema é que *std* possui diversos nomes, e isso provavelmente geraria um possível conflito nas variáveis.
+                Solução -> *using std::cout* *using std::string*
+                    Eu creio que seja melhor utilizar *std::* normalmente do que ficar utilizando esse namespace que pode gerar altas dores de cabeça.
      */
     int x = 5;
     std::cout << x << std::endl;
@@ -196,11 +243,5 @@ int main()
     */
 
 
-    /*
-        INPUT:
-            *std::cin >> variable_name*
-            *std::getline(std::cin >> std::ws, variable_name)* <- Para strings com possíveis whitespaces.
-    */
-    
 
     }
