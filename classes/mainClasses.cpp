faa16e3077a42a80a524a9d833acf91c2588c30a
arthor13 <arthor5games@gmail.com>
2025-10-20 08:50:36 -0300

    OOP classes

--- classes/mainClasses.cpp
+++ classes/mainClasses.cpp
@@ -4,6 +4,22 @@
 class Student
 {
 public:
+    Student(const std::string& studentName, int studentID, int  sudentYear)
+        //AQUI INICIALIZA DA FORMA PADRÃO NO C++
+        : name{studentName}, id{studentID}, year{sudentYear}
+    {
+       ///Forma arcaica de se fazer ->
+       ///name = studentName; ... e por ai vai
+    }
+    /// É possivel criar mais de um constructor desde que a assinatura/parametro seja diferente das outras
+    ///É POSSIVEL CHAMAR UM CONSTRUTOR DENTRO DE OUTRO CONSTRUTOR
+    Student (const char* nameArray, int StudentID, int studentYear)
+        :Student{std::string{nameArray}, StudentID, studentYear}
+        {
+
+        }
+
+
     std::string name;
     int id;
     int year;
@@ -11,14 +27,8 @@ public:
 
 int main()
     {
-    Student studentOne, studentTwo;
-    studentOne.name = "Sam";
-    studentOne.id = 0001;
-    studentOne.year = 1;
-
-    studentTwo.name = "Jão";
-    studentTwo.id = 0002;
-    studentTwo.year = 2;
+    Student studentOne{"Jão", 1234, 1};
+    Student studentTwo{"John", 1235, 1};
 
     std::vector<Student> students = {studentOne, studentTwo};
 
