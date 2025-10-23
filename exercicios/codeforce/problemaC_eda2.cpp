417d180d7ac6a613a42d3d16788ff424cffcd7ea
Arthur de Lima Sobreira <112632734+arthor13@users.noreply.github.com>
2025-10-23 10:01:10 -0300

    Refactor card management functions
    Refactor card handling functions and clean up code.

--- exercicios/codeforce/problemaC_eda2.cpp
+++ exercicios/codeforce/problemaC_eda2.cpp
@@ -1,78 +1,45 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
-vector<int> cartas[51];
-vector<int> ordem;
-int distintos;
-
-
-void trocarCartas(const int& color)
-{
-    /*
-    int i = cartas[color][0];
-    int a = -1;
-    ordem.insert(ordem.begin(), color);
-    ordem.erase(cartas[color].begin()+ i);
-    if (i <= 2)
-    {
-        for (int j = 0; j < 2; j++)
-        {
-            cartas[ordem[j]][0] ++;
-        }
-    }
-    else if ( i > 2 )
-    {
-        for (auto& posicoes : cartas)
-        {
-            a++;
-            for (auto& cor : posicoes)
-            {
-                cout << a << ' ' << cor << '\n';
-            }
-        }
-
-    }*/
-}
-
-int imprimirCartas(const int& color)
-{
-    int a = cartas[color][0];
-    trocarCartas(color);
-    return a;
-
-}
-
+int cartas[51];
 
 void adicionarCartas(const int& n, const int& carta)
 {
-    cartas[carta].push_back(n);
+    if ( cartas[carta] > n )
+    { 
+        cartas[carta] = n;
+
+    }
 }
 
+void trocarCartas(int posicao, int& cor)
+{
+    for(int& posicoes : cartas)
+    {
+        if(posicoes < posicao) posicoes++;
+        else if(posicoes == posicao) posicoes = 1;
+    }
+}
 
+void imprimirCartas(int& cor)
+{
+    cout << cartas[cor]<< '\n';
+    trocarCartas(cartas[cor], cor);
+}
 
 int main()
 {
+    for(int& i : cartas) i = 300000;
     int n, q, card, color;
     cin >> n >> q;
     for( int i = 1; i <= n; i++)
     {
         cin >> card;
         adicionarCartas(i, card);
-        ordem.push_back(card);
     }
-    for ( int i = 1; i <= q; i ++)
+    for ( int i = 0; i < q; i ++)
     {
         cin >> color;
-        cout << imprimirCartas(color) << ' ';
+        imprimirCartas(color);
     }
-    int a = -1;
-    for (auto& c : cartas)
-        {
-            a++;
-            for (auto& q : c) cout << a << ' ' << q <<'\n' ;
-        }
-    cout << "Vetor ordem " << '\n';
-    for (auto c : ordem) cout << c << '\n';
-
 }
